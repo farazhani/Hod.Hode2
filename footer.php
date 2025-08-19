a521620757b0425978643ada7bd2f1a6ce39daf2
farazhani <hanifaraz82@gmail.com>
2025-08-19 19:08:45 +0330

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,22 +1,26 @@
-   <?php wp_footer(); ?>
+
 </body>
-</br>
-</br>
-   <footer class="left-0 right-0 top-0 z-40 w-full shadow bg-white gap-3 flex ">
-      <div class="px-4 max-w-screen-lg mx-auto flex">
-         <div class="flex">
-                       <?php if (function_exists("the_custom_logo")) {
-                the_custom_logo();
-            }
-            ?> 
-         <div><p> کلیه حقوق این سایت برای پارت محفوظ میباشد</p></div>
-         </div>
-         <div class="flex">
-            <img class="w-10 h-auto" src="<?php echo get_template_directory_uri();?>/t.jpg">
-             <img class="w-10 h-auto" src="<?php echo get_template_directory_uri();?>/in.jpg">
-              <img class="w-10 h-auto" src="<?php echo get_template_directory_uri();?>/f.jpg">
-         </div>
-      </div>
-   </footer>
+   <?php wp_footer(); ?>
 </hr>
+   <footer class="bg-white left-0 right-0 top-0 z-40 w-full shadow bg-gray-500"  >
+      <div class="flex ">
+       <div class="max-w-screen-lg mx-auto flex space-x-60">
+       <div class="w-10 h-auto">
+        <?php if(function_exists("the_custom_logo")) 
+        {
+            the_custom_logo();
+        }
+        ?>
+       </div>
+       <p class="text-center text-sm">کلیه حقوق این  سایت برای پارت محفوظ میباشد</p>
+    
+    <div class="flex" >
+         <img class="w-10 h-auto " src="<?php echo get_template_directory_uri();?>/t.jpg">
+                  <img class="w-10 h-auto" src="<?php echo get_template_directory_uri();?>/f.jpg">
+                           <img class="w-10 h-auto" src="<?php echo get_template_directory_uri();?>/in.jpg"> 
+
+    </div>
+    </div>
+      </div>
+    </footer>
 </html>
